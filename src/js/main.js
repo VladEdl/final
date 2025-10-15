50c2db40f86e4d4fa3cb86a6e41d74be76d92848
Vlad <edl.vlad23@gmail.com>
2025-10-15 23:45:34 +0300

    update first page for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import '../scss/style.scss'
+
+
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+// Імпортуємо стилі Swiper
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// Ініціалізуємо Swiper для нашого блоку .team-slider
+const swiper = new Swiper('.team-slider', {
+  modules: [Pagination], // Підключаємо модуль для точок
+  loop: true, // Робимо слайдер безкінечним
+
+  // Налаштовуємо точки
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Робимо їх клікабельними
+},
+});
